b68eb6dfd7496a3b8d1fa20278e7eab99f4ee79c
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-09-15 16:28:48 +0200

    correctly find direct horizontal vertical neighbaour

--- mapbuilder.py
+++ mapbuilder.py
@@ -20,7 +20,7 @@ def load_all_tiles()->dict:
                 "south":float(south), "east":float(east), "name":file}})
     return tile_dic
 
-def build_image_from_tiles(tile_dic:dict)->None:
+def build_tile_graph(tile_dic:dict)->None:
     """build a full sized image from thiles"""
     Graph = nx.Graph()
     Graph.add_nodes_from(tile_dic)
@@ -28,13 +28,19 @@ def build_image_from_tiles(tile_dic:dict)->None:
     for home in Graph.nodes.items():
         for neighbor in Graph.nodes.items():
             if home != neighbor:
-                if (home[1]["north"] == neighbor[1]["south"]):
-                    Graph.add_edge(home[0], neighbor[0])
+                if ((home[1]["north"] == neighbor[1]["south"]) or\
+                    (home[1]["south"] == neighbor[1]["north"]))\
+                    and (home[1]["east"] == neighbor[1]["east"]):
+                        Graph.add_edge(home[0], neighbor[0])
 
+                if ((home[1]["west"] == neighbor[1]["east"]) or\
+                    (home[1]["east"] == neighbor[1]["west"]))\
+                    and (home[1]["north"] == neighbor[1]["north"]):
+                        Graph.add_edge(home[0], neighbor[0])
     print(Graph)
     nx.draw(Graph, with_labels=True, font_weight='bold')
     plt.show()
 
 if __name__ == "__main__":
     tile_dic = load_all_tiles()
-    build_image_from_tiles(tile_dic)
+    build_tile_graph(tile_dic)
