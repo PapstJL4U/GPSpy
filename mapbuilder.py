237a31185b5dd625260f61435b923dd2b144da82
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-09-19 16:11:13 +0200

    added method to find max values in tile dictionary

--- mapbuilder.py
+++ mapbuilder.py
@@ -1,4 +1,4 @@
-from tkinter import E
+from typing import Dict
 import pandas as pd
 from matplotlib import image as mimage
 from matplotlib import pyplot as plt
@@ -20,7 +20,7 @@ def load_all_tiles()->dict:
                 "south":float(south), "east":float(east), "name":file}})
     return tile_dic
 
-def build_tile_graph(tile_dic:dict)->None:
+def build_tile_graph(tile_dic:dict)->nx.Graph:
     """build a full sized image from thiles"""
     Graph = nx.Graph()
     Graph.add_nodes_from(tile_dic)
@@ -28,19 +28,39 @@ def build_tile_graph(tile_dic:dict)->None:
     for home in Graph.nodes.items():
         for neighbor in Graph.nodes.items():
             if home != neighbor:
-                if ((home[1]["north"] == neighbor[1]["south"])\
-                    or (home[1]["south"] == neighbor[1]["north"]))\
+                if (home[1]["north"] == neighbor[1]["south"])\
                     and (home[1]["east"] == neighbor[1]["east"]):
-                        Graph.add_edge(home[0], neighbor[0], orientation="lat")
-
-                if ((home[1]["west"] == neighbor[1]["east"])\
-                    or (home[1]["east"] == neighbor[1]["west"]))\
+                        Graph.add_edge(home[0], neighbor[0], orientation="South")
+                if (home[1]["south"] == neighbor[1]["north"])\
+                    and (home[1]["east"] == neighbor[1]["east"]):
+                        Graph.add_edge(home[0], neighbor[0], orientation="North")
+                
+                if (home[1]["west"] == neighbor[1]["east"])\
                     and (home[1]["north"] == neighbor[1]["north"]):
-                        Graph.add_edge(home[0], neighbor[0], orientation="lon")
+                        Graph.add_edge(home[0], neighbor[0], orientation="East")
+                if (home[1]["east"] == neighbor[1]["west"])\
+                    and (home[1]["north"] == neighbor[1]["north"]):
+                        Graph.add_edge(home[0], neighbor[0], orientation="West")
+    
     print(Graph)
     nx.draw(Graph, with_labels=True, font_weight='bold',)
     plt.show()
+    return Graph
+def extreme_values(tile_dic:dict):
+    max_lat, max_lon, min_lat, min_lon = -91, -181, 91, 181
+
+    for item in tile_dic.items():
+        if item[1]["north"] > max_lon:
+            max_lon = item[1]["north"]
+        if item[1]["south"] < min_lon:
+            min_lon = item[1]["south"]
+        if item[1]["east"] < min_lat:
+            min_lat = item[1]["east"]
+        if item[1]["west"] > max_lat:
+            max_lat = item[1]["west"]
+    return max_lat, max_lon, min_lat, min_lon
 
 if __name__ == "__main__":
-    tile_dic = load_all_tiles()
-    build_tile_graph(tile_dic)
+    tile_dic:Dict = load_all_tiles()
+    Graph:nx.Graph = build_tile_graph(tile_dic)
+    print(extreme_values(tile_dic))
