265b0515fb26102586484f712d7915bb386ef111
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-09-20 10:30:04 +0200

    renamed method new method to find tile size in degree

--- mapbuilder.py
+++ mapbuilder.py
@@ -1,4 +1,5 @@
-from typing import Dict
+"""Loads the tiles, builds a graph and finds the boundaries of the map"""
+from math import cos
 import pandas as pd
 from matplotlib import image as mimage
 from matplotlib import pyplot as plt
@@ -46,21 +47,44 @@ def build_tile_graph(tile_dic:dict)->nx.Graph:
     nx.draw(Graph, with_labels=True, font_weight='bold',)
     plt.show()
     return Graph
-def extreme_values(tile_dic:dict):
-    max_lat, max_lon, min_lat, min_lon = -91, -181, 91, 181
+
+def map_boundaries(tile_dic:dict)->tuple:
+    """return the boundaries of the map based on the used tiles"""
+    max_lat, max_lon, min_lat, min_lon = -100.0, -200.0, 100.0, 200.0
 
     for item in tile_dic.items():
-        if item[1]["north"] > max_lon:
-            max_lon = item[1]["north"]
-        if item[1]["south"] < min_lon:
-            min_lon = item[1]["south"]
-        if item[1]["east"] < min_lat:
-            min_lat = item[1]["east"]
-        if item[1]["west"] > max_lat:
-            max_lat = item[1]["west"]
-    return max_lat, max_lon, min_lat, min_lon
+        if item[1]["north"] > max_lat:
+            max_lat = item[1]["north"]
+        if item[1]["south"] < min_lat:
+            min_lat = item[1]["south"]
+        if item[1]["east"] > max_lon:
+            max_lon = item[1]["east"]
+        if item[1]["west"] < min_lon:
+            min_lon = item[1]["west"]
+    return (max_lat, max_lon, min_lat, min_lon)
+
+def avg_tile_size_degree(tile_dict:dict)->int:
+    
+    list_lat, list_lon = [], []
+
+    for item in tile_dict.items():
+        west, east = item[1]["west"], item[1]["east"]
+        north, south = item[1]["north"], item[1]["south"]
+        distance_lat = abs(north - south)
+        distance_lon = abs(east - west)
+        list_lat.append(distance_lat)
+        list_lon.append(distance_lon)
+
+    average_lat = sum(list_lat)/len(list_lat)
+    average_lon = sum(list_lon)/len(list_lon)
+    
+    return average_lat, average_lon
 
 if __name__ == "__main__":
-    tile_dic:Dict = load_all_tiles()
-    Graph:nx.Graph = build_tile_graph(tile_dic)
-    print(extreme_values(tile_dic))
+    tile_dic:dict = load_all_tiles()
+    #Graph:nx.Graph = build_tile_graph(tile_dic)
+    max_lat, max_lon, min_lat, min_lon = map_boundaries(tile_dic)
+    average_lat, average_lon = avg_tile_size_degree(tile_dic)
+    mla = round((max_lat-min_lat) / average_lat,0)
+    mlo = round((max_lon-min_lon) / average_lon,0)
+    print(mla, mlo)
