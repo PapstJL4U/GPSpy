37251e3b05d7f2c0dfd36c4649a3aefa59dac02b
PapstJL4U <PapstJL4U@users.noreply.github.com>
2022-09-23 15:19:54 +0200

    better uplad

--- server/server.py
+++ server/server.py
@@ -1,10 +1,11 @@
+import logging
 from bottle import static_file, run, template, request, Bottle
 import os
 from pathlib import Path
 
 app = Bottle()
 home:Path = Path.home()
-safe_location = home.join("server", "temp_data")
+safe_location = home.joinpath("server", "temp_data")
 
 @app.route('/')
 @app.route('/hello')
@@ -15,36 +16,27 @@ def ind():
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-@app.get('/gps/simple')
+@app.route('/gps/simple', method='GET')
 def get_a_map():
-    return None
-
-@app.post('/gps/simple')
-def give_a_route():
-    return None
-
-@app.get('/gps/detailed')
-def get_a_detailed_map():
-    return None
-
-@app.post('/gps/detailed')
-def give_a_detailed_route():
-    return None
+    return """<form action="/gps/simple" method="post" enctype="multipart/form-data">
+                Select a file: <input type="file" name="upload" />
+                <input type="submit" value="Start upload" />
+                </form>"""
 
 @app.route('/gps/<filename:path>')
 def send_static(filename):
     return static_file(filename, root='/path/to/static/files')
 
-@app.route('/upload', method='POST')
+
+@app.route('/gps/simple', method='POST')
 def do_upload():
-    category   = request.forms.get('category')
     upload     = request.files.get('upload')
     name, ext = os.path.splitext(upload.filename)
-    if ext not in ('.csv'):
+    if ext != '.csv':
         return 'File extension not allowed.'
-
-    save_path = safe_location.join(name)
+    save_path = safe_location.joinpath(name).resolve().as_uri
     upload.save(save_path) # appends upload.filename automatically
-    return 'OK'
+    test = safe_location.resolve().as_uri
+    return "OK"
 
 run(app, host='localhost', port=21812)
